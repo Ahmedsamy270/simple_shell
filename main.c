e03d6462f46c9cc4a18f061380e4be7e43d3fcc8
toka magdy <tokamagdy545@gmail.com>
2023-12-11 10:20:55 +0200

    main

---
+++ main.c
@@ -0,0 +1,18 @@
+#include "simple_shell.h"
+
+/**
+ * main - is the main function for all simple shell project
+ * Return: 0 On Success
+ */
+int main(void)
+{
+	if (isatty(STDIN_FILENO) == 1)
+	{
+		actmode();
+	}
+	else
+	{
+		non_actmode();
+	}
+	return (0);
+}
