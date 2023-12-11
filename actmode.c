d15ef74026279f47c159c24856afb5e0fb16075b
toka magdy <tokamagdy545@gmail.com>
2023-12-11 10:36:22 +0200

    act

---
+++ actmode.c
@@ -0,0 +1,25 @@
+#include "simple_shell"
+
+/**
+ * actmode - is a function that workes in interactive mode
+ * Return: Void
+ */
+void actmode(void)
+{
+	char *ptrline;
+	char **arg;
+	int report = -1;
+
+	do {
+		printf("my_shell$ ");
+		ptrline = line_reading();
+		arg = line_div(ptrline);
+		report = arg_exe(arg);
+		free(ptrline);
+		free(arg);
+		if (report >= 0)
+		{
+			exit(report);
+		}
+	} while (report == -1);
+}
